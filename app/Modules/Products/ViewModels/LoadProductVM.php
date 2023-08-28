2a016c4a806dc11f5fb98276598dff6c1d7726c1
AliSuliman <ali.sliman@ramaaz.com>
2023-08-29 00:38:30 +0300

    - refactor the code

---
+++ app/Modules/Products/ViewModels/LoadProductVM.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Modules\Products\ViewModels;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+class LoadProductVM implements Arrayable
+{
+    private $product;
+
+    public function __construct($product)
+    {
+        $this->product = $product->load([
+            'images',
+            'translations',
+            'categories',
+        ]);
+    }
+
+    public function toArray()
+    {
+        return $this->product;
+    }
+}
