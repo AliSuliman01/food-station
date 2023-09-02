20d9e6088e7cac24da8bcbfb4016c7f9846ab4f5
AliSuliman <ali.sliman@ramaaz.com>
2023-09-03 01:05:51 +0300

    - add POPULAR_PRODUCTS category

--- app/Enums/CategoryEnum.php
+++ app/Enums/CategoryEnum.php
@@ -8,11 +8,14 @@ use BenSampo\Enum\Enum;
 
 /**
  * @method static static AVAILABLE_TODAY()
+ * @method static static POPULAR_PRODUCTS()
  * @method static static INGREDIENTS()
  */
 final class CategoryEnum extends Enum
 {
     const AVAILABLE_TODAY = 'AVAILABLE_TODAY';
+
+    const POPULAR_PRODUCTS = 'POPULAR_PRODUCTS';
 
     const INGREDIENTS = 'INGREDIENTS';
 }
