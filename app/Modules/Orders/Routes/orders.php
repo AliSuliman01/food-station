b7ecd3d9eb29cb15b41969720eadb1ffb0df281c
AliSuliman <ali.sliman@ramaaz.com>
2023-09-11 08:49:16 +0300

    - fix get user orders route uri.

--- app/Modules/Orders/Routes/orders.php
+++ app/Modules/Orders/Routes/orders.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('orders')
     ->middleware('auth:api')
     ->group(function () {
-        Route::get('userOrder/{user}', [OrderController::class, 'userOrders']);
+        Route::get('user_orders/{user}', [OrderController::class, 'userOrders']);
         Route::post('store_custom', [OrderController::class, 'store_custom']);
     });
 Route::middleware('auth:api')
